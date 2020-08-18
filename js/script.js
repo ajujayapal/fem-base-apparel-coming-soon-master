fd0caf66d008d6ac9e15596700db37cf4088701c
Aju Jayapalan <aju.jayapal@gmail.com>
2020-08-17 22:36:26 -0500

    Mobile Layout V1.0 done

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const divButton = document.querySelector(".form-field span.btn-arrow");
+const divErrorIcon = document.querySelector(".form-field span.icon-error");
+const divErrorField = document.querySelector(".form-field span.error-message");
+const divSuccessField = document.querySelector(
+  ".form-field span.success-message"
+);
+const emailField = document.querySelector("#email");
+
+emailField.addEventListener("input", () => clear());
+
+divButton.addEventListener("click", handleButtonClick);
+divButton.addEventListener("animationend", handleAnimationEnd);
+
+function handleButtonClick() {
+  let email = emailField.value;
+  let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!pattern.test(email)) {
+    showErrorElements();
+  } else {
+    hideErrorElements();
+    divButton.classList.add("btn-slide");
+    emailField.value = "";
+  }
+}
+
+function handleAnimationEnd() {
+  divButton.classList.remove("btn-slide");
+  divSuccessField.style.display = "block";
+}
+
+function showErrorElements() {
+  divErrorIcon.style.display = "block";
+  divErrorField.style.display = "block";
+}
+
+function hideErrorElements() {
+  divErrorIcon.style.display = "none";
+  divErrorField.style.display = "none";
+}
+
+function clear() {
+  hideErrorElements();
+  divSuccessField.style.display = "none";
+}
